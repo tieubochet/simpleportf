10819301e9f451c15e3c90f78ff6348ff2e33f82
tieubochet <tao.nguyen712@gmail.com>
2025-07-31 13:52:42 +0700

    v1.3

--- types.ts
+++ types.ts
@@ -5,6 +5,7 @@ export interface Transaction {
   quantity: number;
   pricePerUnit: number; // Price in USD. For transfers, this will be 0.
   date: string; // ISO string for the date of the transaction
+  notes?: string; // Optional field for user notes
 }
 
 // Represents an asset held by the user, composed of multiple transactions
