806e4db378545b72c07735f8672bba92dcd2aa5f
tieubochet <tao.nguyen712@gmail.com>
2025-08-01 07:46:58 +0700

    change performance & allocation style

--- components/icons.tsx
+++ components/icons.tsx
@@ -114,3 +114,11 @@ export const ArrowDownIcon: React.FC<IconProps> = (props) => (
       <path d="M12 5v14m7-7l-7 7-7-7"/>
     </svg>
 );
+
+export const InfoIcon: React.FC<IconProps> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+        <circle cx="12" cy="12" r="10"></circle>
+        <line x1="12" y1="16" x2="12" y2="12"></line>
+        <line x1="12" y1="8" x2="12.01" y2="8"></line>
+    </svg>
+);
