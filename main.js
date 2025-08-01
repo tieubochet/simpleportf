279086ba69de6ffa7fd4e5362c4fa6625dfb44a2
tieubochet <tao.nguyen712@gmail.com>
2025-08-01 09:54:24 +0700

    v1.5

---
+++ main.js
@@ -0,0 +1,4 @@
+// This file acts as a bridge between the HTML and the TSX entrypoint
+// to help build tools that might get confused by a direct .tsx import
+// in an HTML file.
+import './index.tsx';
