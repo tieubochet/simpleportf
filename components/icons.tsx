58b854c41d3fca2131377a6f0c0650114dff6e9b
tieubochet <tao.nguyen712@gmail.com>
2025-08-01 09:22:34 +0700

    v1.5

--- components/icons.tsx
+++ components/icons.tsx
@@ -136,3 +136,11 @@ export const EyeOffIcon: React.FC<IconProps> = (props) => (
         <line x1="1" y1="1" x2="23" y2="23"></line>
     </svg>
 );
+
+export const SparklesIcon: React.FC<IconProps> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+        <path d="m12 3-1.9 3.8-3.8 1.9 3.8 1.9L12 14.4l1.9-3.8 3.8-1.9-3.8-1.9L12 3z"/>
+        <path d="M5 12.5 6.9 16l3.1-1.9L6.9 12.2 5 12.5z"/>
+        <path d="m17.1 8-3.1 1.9 1.9 3.1 1.2-1.9L17.1 8z"/>
+    </svg>
+);
