fed2eace07240e0207034df8959289169667b9e5
tieubochet <tao.nguyen712@gmail.com>
2025-08-09 11:13:04 +0700

    change icon 24hchange

--- components/icons.tsx
+++ components/icons.tsx
@@ -136,3 +136,10 @@ export const EyeOffIcon: React.FC<IconProps> = (props) => (
         <line x1="1" y1="1" x2="23" y2="23"></line>
     </svg>
 );
+
+export const ClockIcon: React.FC<IconProps> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+      <circle cx="12" cy="12" r="10"></circle>
+      <polyline points="12 6 12 12 16 14"></polyline>
+    </svg>
+);
